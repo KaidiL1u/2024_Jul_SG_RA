42ff9396cdead158c4364b4f1c9a526a2ff9781e
Kaidi Liu <kaidi.lau@outlook.com>
2024-07-17 18:14:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ class RegressionApp:
     def choose_file(self):
         file = st.file_uploader("Upload Excel file", type=["xlsx"])
         if file:
-            self.df = pd.read_excel(file, sheet_name="Sheet1")
+            self.df = pd.read_excel(file, sheet_name="Sheet1", dtype=float)
             self.variables = self.df.columns[2:].tolist()  # Assuming variables start from column C onwards
             st.write("### Columns in the uploaded file:")
             st.write(self.df.columns.tolist())
